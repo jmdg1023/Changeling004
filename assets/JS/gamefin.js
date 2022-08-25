88690d33b45aefa04633246de96d0b51dbfc276b
Astrid <jmdg1023@gmail.com>
2022-08-25 21:55:02 +1000

    debugging

--- assets/JS/gamefin.js
+++ assets/JS/gamefin.js
@@ -1,2 +1,38 @@
-var mostRecentScore = localStorage.getItem('mostRecentScore')
-document.getElementById('score').textContent= mostRecentScore;
+var mostRecentScore = localStorage.getItem('mostRecentScore');
+document.getElementById('score').innerText= mostRecentScore;
+
+var initials =[];
+var highscoresList = document.querySelector("#highscoresList");
+var initialsForm = document.querySelector("#initialsForm");
+var username = document.querySelector("#username");
+function storeInitials() {
+
+    localStorage.setItem("initials", JSON.stringify(initials));
+  }
+  
+    initialsForm.addEventListener("submit", function(event) {
+        window.location.assign("./highscores.html")
+    var highscoresList = username.value.trim();
+    if (highscoresList === "") {
+      return;
+    }
+   todos.push(highscoresList);
+    todoInput.value = "";
+   
+    storeInitials();
+    renderInitials();
+  });
+
+  function renderInitials() {
+    highscoresList.innerHTML = "";
+   for (var i = 0; i < initials.length; i++) {
+      var Ini = initials[i];
+  
+      var li = document.createElement("li");
+      li.textContent = Ini;
+      li.setAttribute("data-index", i);
+  
+    
+      highscoresList.appendChild(li);
+    }
+  }
